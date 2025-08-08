a7af3d26168c7b0b5d76d7c7bbd83e4af6bfedb4
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-08-07 22:21:00 -0300

    feat: add task data manipulating

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -5,10 +5,16 @@ import { InputCheckbox } from "../components/input-checkbox";
 import { Text } from "../components/text";
 import { useState } from "react";
 import { InputText } from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export function TaskItem(){
+interface TaskItemProps {
+    task: Task
+}
 
-    const [isEditing, setIsEditing] = useState(false)
+export function TaskItem({task}:TaskItemProps){
+
+    const [isEditing, setIsEditing] = useState(task?.state === TaskState.Creating)
 
     function handleEditTask() {
         setIsEditing(true)
@@ -22,8 +28,8 @@ export function TaskItem(){
         <Card size="md" className="flex items-center gap-4">
             {!isEditing ? <>
             
-            <InputCheckbox />
-            <Text className="flex-1">Desenvolver sistema</Text>
+            <InputCheckbox value={task?.isComplete?.toString()} checked={task?.isComplete} />
+            <Text className={cx("flex-1", {"line-through opacity-85": task?.isComplete})}>{task?.title}</Text>
             <div className="flex items-center justify-center gap-1">
                 <IconButton variant="ghost" icon={<Trash size={16} />}/>
                 <IconButton variant="ghost" icon={<Pencil size={16} onClick={handleEditTask} />}/>
--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,21 +1,33 @@
 import { Plus } from "lucide-react";
 import { Button } from "../components/button";
 import { TaskItem } from "./task-item";
+import { useTasks } from "../hooks/use-tasks";
+import { useTask } from "../hooks/use-task";
 
 export function TasksList() {
+
+    const { tasks } = useTasks()
+    const { prepareTask } = useTask()
+
+    function handlePrepareTask(){
+        prepareTask()
+    }
+
+    console.log(tasks)
+
     return (
         <>
             <section>
-                <Button variant="secondary" className="flex justify-center items-center w-full text-md font-bold h-20">
+                <Button 
+                variant="secondary"
+                onClick={handlePrepareTask}
+                className="flex justify-center items-center w-full text-md font-bold h-20">
                    <Plus /> 
                         Nova tarefa
                 </Button>
             </section>
             <section className=" flex flex-col space-y-4">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
+                {tasks.map((task) => <TaskItem key={task.id} task={task} />)}
             </section>
         </>
     )
